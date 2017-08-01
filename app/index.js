b12f0e45ec99f2c683e82fe7969c72345ddd2c55
Ryan Bryce <ryan.bryce1992@gmail.com>
2017-08-01 11:43:54 -0600

    using PropTypes and understaanding pure functions in JS, the react render method needs to have pure functions

--- app/index.js
+++ app/index.js
@@ -1,18 +1,49 @@
 var React = require('react');
-var ReactDom = require('react-dom');
+var ReactDOM = require('react-dom');
+const PropTypes = require('prop-types');
 require('./index.css');
 
-class App extends React.Component{
+class Users extends React.Component {
   render() {
     return (
       <div>
-        yooooo
+        <h1>Friends</h1>
+        <ul>
+          {this.props.list.filter(function (th){
+            return th.friend === true
+          }).map( function (hmm) {
+            return <li key={hmm.name}> {hmm.name} </li>
+          })}
+        </ul>
+
+        <hr />
+
+        <h1> Non Friends </h1>
+        <ul>
+          {this.props.list.filter(function (th){
+            return th.friend !== true
+          }).map( function (hmm) {
+            return <li key={hmm.name}> {hmm.name} </li>
+          })}
+        </ul>
       </div>
     )
   }
 }
-
-ReactDom.render(
-  <App />,
-document.getElementById('app')
+Users.propTypes = {
+  list: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    friend: PropTypes.bool.isRequired
+  }))
+}
+ReactDOM.render(
+  <Users list={[
+    { name: 'Tyler', friend: true },
+    { name: 'Ryan', friend: true },
+    { name: 'Michael', friend: false },
+    { name: 'Mikenzi', friend: false },
+    { name: 'Jessica', friend: true },
+    { name: 'Dan', friend: false } ]}
+  />,
+  document.getElementById('app')
 );
