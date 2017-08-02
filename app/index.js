b9787928ce7a535b249dfb8f088c7c5d33cca0b7
Ryan Bryce <ryan.bryce1992@gmail.com>
2017-08-02 10:01:53 -0600

    made a stater navigation compnent for hitting different parts of the github api. used .map() to displat the data in an array and added state to the component and locked down the components this keyword so it always points to the correct obj using .bind.

--- app/index.js
+++ app/index.js
@@ -1,49 +1,10 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-const PropTypes = require('prop-types');
+var PropTypes = require('prop-types');
+var App = require('./components/App')
 require('./index.css');
 
-class Users extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Friends</h1>
-        <ul>
-          {this.props.list.filter(function (th){
-            return th.friend === true
-          }).map( function (hmm) {
-            return <li key={hmm.name}> {hmm.name} </li>
-          })}
-        </ul>
-
-        <hr />
-
-        <h1> Non Friends </h1>
-        <ul>
-          {this.props.list.filter(function (th){
-            return th.friend !== true
-          }).map( function (hmm) {
-            return <li key={hmm.name}> {hmm.name} </li>
-          })}
-        </ul>
-      </div>
-    )
-  }
-}
-Users.propTypes = {
-  list: PropTypes.arrayOf(PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    friend: PropTypes.bool.isRequired
-  }))
-}
 ReactDOM.render(
-  <Users list={[
-    { name: 'Tyler', friend: true },
-    { name: 'Ryan', friend: true },
-    { name: 'Michael', friend: false },
-    { name: 'Mikenzi', friend: false },
-    { name: 'Jessica', friend: true },
-    { name: 'Dan', friend: false } ]}
-  />,
+  <App />,
   document.getElementById('app')
 );
