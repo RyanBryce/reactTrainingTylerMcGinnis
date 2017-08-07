83471409134c082f2491be92a726ee396e390140
Ryan Bryce <ryan.bryce1992@gmail.com>
2017-08-07 07:46:10 -0600

    adding more axios calls

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,5 +1,15 @@
 var axios = require('axios');
 
+function getProfile (username){
+  return axios.get('https://api.github.com/users/' + username + params))
+    .then(function (user) {
+      return user.data;
+    });
+}
+function getRepos (username) {
+  return axios.get('https://api.github.com/users/' + username + '/repos' +  params + '&per_page=100')
+}
+
 module.exports = {
   fetchPopularRepos: function (language) {
     var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories');
